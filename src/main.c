abfec63bebd4654153d62da457c27f68f7993dab
Matt Hocker <mhocker@chikanamakalaka.tk>
2015-05-07 14:21:26 -0700

    more cleanup

--- src/main.c
+++ src/main.c
@@ -283,7 +283,7 @@
 /* Define this to use SDL. To use SDL, you must have SDL headers
  * available and you must link with the SDL library when you compile. */
 /* Comment this out to compile without SDL visualization support. */
-#define USE_SDL 1
+define USE_SDL 1
 
 /* ----------------------------------------------------------------------- */
 
